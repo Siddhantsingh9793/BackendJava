28f50ef1e9e872e81c2b5e96516339f9b7c1859f
siddhant kumar singh <113007815+Siddhantsingh9793@users.noreply.github.com>
2026-01-09 17:24:07 +0530

    first program variables

---
+++ src/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args){
+        int age=20;
+        String name= "siddhant";
+        String language="java";
+
+
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(language);
+
+
+    }
+
+}
