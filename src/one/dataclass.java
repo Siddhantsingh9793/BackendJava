281a434bc2dca97cd00f3b8b246af3175dfdcf3a
siddhant kumar singh <113007815+Siddhantsingh9793@users.noreply.github.com>
2026-01-13 16:58:30 +0530

    data variableNameDemo

---
+++ src/one/dataclass.java
@@ -0,0 +1,15 @@
+package one;
+
+public class dataclass {
+    public static void main(String[]args) {
+        int age = 25;
+        String Name = "Siddhant";
+        String Name1 = "farhan";
+        int age1 = 30;
+        System.out.println("age" + age);
+        System.out.println("name" + Name);
+        System.out.println("name" + Name1);
+        System.out.println("age" + age1);
+
+    }
+}
---
+++ src/one/variableNameDemo.java
@@ -0,0 +1,12 @@
+package one;
+
+public class variableNameDemo {
+    public static void main(String[]args){
+        int num = 10;
+        int Num = 20;
+        System.out.println("num= " + num);
+        System.out.println("Num= " + Num);
+
+
+    }
+}
